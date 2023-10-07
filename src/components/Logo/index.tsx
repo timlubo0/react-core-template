659630531e94d2edb40d97b50798e3e0f25ec135
Tim Lubo <timlubo0@gmail.com>
2023-10-08 01:54:39 +0200

    fix: MantineColorScheme

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,20 +1,20 @@
-import { useMantineTheme } from "@mantine/core";
+import { useMantineColorScheme, useMantineTheme } from "@mantine/core";
 
-interface Props{
-    size?: number;
-    mode?: 'light' | 'dark'
+interface Props {
+  size?: number;
+  mode?: "light" | "dark";
 }
 
 const Logo = ({ size = 70, mode }: Props) => {
-    const theme = useMantineTheme();
-    const colorScheme = mode ? mode : theme.colorScheme;
-    return(
-        <img
-            src={colorScheme === 'light' ? "/icon.png" : "/icon-white.png"}
-            alt="Logo Seguce"
-            width={size}
-        />
-    )
-}
+  const { colorScheme: mColorScheme } = useMantineColorScheme();
+  const colorScheme = mode ? mode : mColorScheme;
+  return (
+    <img
+      src={colorScheme === "light" ? "/icon.png" : "/icon-white.png"}
+      alt="Logo Seguce"
+      width={size}
+    />
+  );
+};
 
 export default Logo;
