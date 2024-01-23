f478e1306baffbbae64b42057facaa6847ef96c5
Tim Lubo <timlubo0@gmail.com>
2024-01-23 16:42:59 +0200

    fix: review for chakra ui integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -5,43 +6,27 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import MainNavigator from "./navigation/MainNavigator";
 import AppLayout from "./layouts/AppLayout";
-import { useBeforeRender } from "./hooks/useBeforeRender";
 import { ReactNode } from "react";
 import { theme } from "src/utils/theme";
 
 const queryClient = new QueryClient();
 
 export default function App({ children }: { children?: ReactNode }) {
-  useBeforeRender(() => {
-    window.addEventListener("error", (e) => {
-      if (e) {
-        const resizeObserverErrDiv = document.getElementById(
-          "webpack-dev-server-client-overlay-div"
-        );
-        const resizeObserverErr = document.getElementById(
-          "webpack-dev-server-client-overlay"
-        );
-        if (resizeObserverErr)
-          resizeObserverErr.className = "hide-resize-observer";
-        if (resizeObserverErrDiv)
-          resizeObserverErrDiv.className = "hide-resize-observer";
-      }
-    });
-  }, []);
-
   return (
-    <MantineProvider theme={theme}>
-      <ModalsProvider>
-        <ToastContainer />
-        <QueryClientProvider client={queryClient}>
-          <Router>
-            <AppLayout>
-              <MainNavigator />
-              {children}
-            </AppLayout>
-          </Router>
-        </QueryClientProvider>
-      </ModalsProvider>
-    </MantineProvider>
+    <ChakraProvider>
+      <MantineProvider theme={theme}>
+        <ModalsProvider>
+          <ToastContainer />
+          <QueryClientProvider client={queryClient}>
+            <Router>
+              <AppLayout>
+                <MainNavigator />
+                {children}
+              </AppLayout>
+            </Router>
+          </QueryClientProvider>
+        </ModalsProvider>
+      </MantineProvider>
+    </ChakraProvider>
   );
 }
