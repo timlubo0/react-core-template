59c96d3b3e0193f0a07e39b9fe52a5b54337d769
Tim Lubo <timlubo0@gmail.com>
2024-01-24 10:27:25 +0200

    fix: review

--- src/components/ImageFileInput.tsx
+++ src/components/ImageFileInput.tsx
@@ -1,32 +1,32 @@
 import { useEffect, useState } from "react";
 import { Text, SimpleGrid, Image } from "./base";
-import { Dropzone, IMAGE_MIME_TYPE, FileWithPath } from "@mantine/dropzone";
+// import { Dropzone, IMAGE_MIME_TYPE, FileWithPath } from "@mantine/dropzone";
 
 interface Props {
-  onChange: (files: FileWithPath[]) => void;
+  // onChange: (files: FileWithPath[]) => void;
   size?: number | string;
   placeholder?: string;
 }
 
 function ImageFileInput({
-  onChange,
+  //onChange,
   size = 150,
   placeholder = "Inserer des images",
 }: Props) {
-  const [files, setFiles] = useState<FileWithPath[]>([]);
+  // const [files, setFiles] = useState<FileWithPath[]>([]);
 
-  const previews = files.map((file, index) => {
-    const imageUrl = URL.createObjectURL(file);
-    return <Image key={index} src={imageUrl} />;
-  });
+  // const previews = files.map((file, index) => {
+  //   const imageUrl = URL.createObjectURL(file);
+  //   return <Image key={index} src={imageUrl} />;
+  // });
 
-  useEffect(() => {
-    onChange(files);
-  }, [files, onChange]);
+  // useEffect(() => {
+  //   onChange(files);
+  // }, [files, onChange]);
 
   return (
     <div>
-      <Dropzone accept={IMAGE_MIME_TYPE} onDrop={setFiles} h={"100%"} w={size}>
+      {/* <Dropzone accept={IMAGE_MIME_TYPE} onDrop={setFiles} h={"100%"} w={size}>
         {previews.length === 0 && <Text>{placeholder}</Text>}
 
         <SimpleGrid
@@ -37,7 +37,7 @@ function ImageFileInput({
         >
           {previews}
         </SimpleGrid>
-      </Dropzone>
+      </Dropzone> */}
     </div>
   );
 }
